53ca48f413b3a33692788723d2b63846f47607ab
DoubleADev <92317399+APROMASTER@users.noreply.github.com>
2025-08-23 13:52:16 -0400

    project imported

---
+++ Assets/_Project/GadgebotGoal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GadgebotGoal : MonoBehaviour
+{
+    [SerializeField, Min(0)] private int _count = 3;
+    public int count
+    {
+        get
+        {
+            return _count;
+        }
+
+        set
+        {
+            _count = value;
+            onCountUpdated?.Invoke(_count);
+        }
+    }
+    public GadgebotUnityEvent onReachedGoal;
+    public IntUnityEvent onCountUpdated;
+
+    public void OnInteract(Gadgebot gadgebot)
+    {
+        onReachedGoal?.Invoke(gadgebot);
+        Destroy(gadgebot.gameObject);
+        if (count > 0) count--;
+    }
+
+    void OnValidate()
+    {
+        onCountUpdated?.Invoke(_count);
+    }
+}
---
+++ Assets/_Project/UnityEventHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+[Serializable]
+public class GameObjectUnityEvent : UnityEvent<GameObject> { }
+
+[Serializable]
+public class GadgebotUnityEvent : UnityEvent<Gadgebot> { }
+
+[Serializable]
+public class IntUnityEvent : UnityEvent<int> { }
