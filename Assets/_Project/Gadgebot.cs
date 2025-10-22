1fb41b0acea7e8386a7313f038ac66be4130fac1
APROMASTER <arielnunezmendez@gmail.com>
2025-10-22 16:14:35 -0400

    Going back in time

---
+++ Assets/_Project/Gadgebot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gadgebot : MonoBehaviour 
+{
+	public float speed = 1;
+	public Rigidbody2D physics;
+	public GadgebotCommandState currentCommand;
+
+	void Start () 
+	{
+		
+	}
+	
+	void FixedUpdate () 
+	{
+		physics.velocity = Vector3.right * speed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.TryGetComponent(out GadgebotGoal goal))
+		{
+			goal.OnInteract(this);
+		}
+	}
+
+	public void ChangeCommandState(GadgebotCommandState commandState)
+	{
+
+	}
+}
+
+public abstract class GadgebotCommandState
+{
+	public abstract void OnCommandStateEnter(Collider other);
+	public abstract void OnCommandStateExit(Collider other);
+	public abstract void OnTriggerEnter(Collider other);
+	public abstract void TrySetCommand(GadgebotCommandState command);
+}
---
+++ Assets/_Project/GadgebotSurvivalGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GadgebotSurvivalGame : MonoBehaviour 
+{
+	public GadgebotSpawner spawner;
+	public GadgebotGoal goal;
+	public GameObject winPopup; 
+
+	void Awake ()
+	{
+		goal.onCountUpdated.AddListener(CheckWin);
+	}
+
+	public void StartGame()
+	{
+		
+	}
+
+	public void CheckWin(int count)
+	{
+		if (count != 0) return;
+
+		winPopup.SetActive(true);
+	}
+
+	void OnDestroy ()
+	{
+		goal.onCountUpdated.RemoveListener(CheckWin);
+	}
+}
